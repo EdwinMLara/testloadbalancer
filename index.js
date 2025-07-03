c06a83dfda8f072a7c742fb7df67c76d0b9e8110
EdwinMLara <emlara35@gmail.com>
2025-07-03 04:02:59 -0600

    more

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Ubuntu EC2! with the runner');
+  res.send('Hello from Ubuntu EC2! with the runner2');
 });
 
 app.listen(port, () => {
